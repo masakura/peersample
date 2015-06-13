b6fe07ec36730f678a929cc71506c0512a01536d
Tomo Masakura <tomo.masakura@gmail.com>
2015-06-13 13:51:18 +0900

    Use video

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+(function () {
+  'use strict';
+
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+})();
+
+$(document).ready(function () {
+  'use strict';
+
+  navigator.getUserMedia({audio: false, video: true}, function (stream) {
+    $('#video').prop('src', URL.createObjectURL(stream));
+  }, function (error) {
+    console.log(error);
+  });
+});
