b94d8e9d2f48cdb13e6eee9c1d9bbc42b69d31f5
Tomo Masakura <tomo.masakura@gmail.com>
2015-06-13 14:39:21 +0900

    Refectoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,15 +10,6 @@
 $(document).ready(function () {
   'use strict';
 
-  var myStream;
-
-  navigator.getUserMedia({audio: false, video: true}, function (stream) {
-    $('#video').prop('src', URL.createObjectURL(stream));
-    myStream = stream;
-  }, function (error) {
-    console.log(error);
-  });
-
   var log = function (text) {
     $('<p>').text(text).appendTo('#messages');
   };
@@ -27,18 +18,45 @@ $(document).ready(function () {
     $('#other-video').prop('src', URL.createObjectURL(stream));
   };
 
-  var peer = new Peer({key: 'vvx00u5ssce1xlxr'});
-  peer
-    .on('open', function (id) {
-      log(id);
-    })
-    .on('call', function (call) {
-      call.answer(myStream);
-      call.on('stream', showOtherStream);
-    });
+  var phone = (function () {
+    var myStream;
+    var peer;
+
+    return {
+      start: function () {
+        navigator.getUserMedia({audio: false, video: true}, function (stream) {
+          $('#video').prop('src', URL.createObjectURL(stream));
+          myStream = stream;
+        }, function (error) {
+          log(error);
+        });
+
+        peer = new Peer({key: 'vvx00u5ssce1xlxr'});
+
+        peer.on('error', function (error) {
+          log(JSON.stringfy(error));
+        });
+        peer.on('open', function (id) {
+          log(id);
+        });
+        peer.on('call', function (call) {
+          this.answer(call);
+        }.bind(this));
+      },
+      call: function (id) {
+        var call = peer.call(id, myStream);
+        call.on('stream', showOtherStream);
+      },
+      answer: function (call) {
+        call.answer(myStream);
+        call.on('stream', showOtherStream);
+      }
+    };
+  })();
+
+  phone.start();
 
   $(document).on('click', '#call', function () {
-    var call = peer.call($('#other-peerid').val(), myStream);
-    call.on('stream', showOtherStream);
+    phone.call($('#other-peerid').val());
   });
 });
