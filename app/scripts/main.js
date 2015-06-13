34da82b23793156c2ec2b72cf60edd2650b2c927
Tomo Masakura <tomo.masakura@gmail.com>
2015-06-13 14:23:26 +0900

    Can call

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,9 +10,35 @@
 $(document).ready(function () {
   'use strict';
 
+  var myStream;
+
   navigator.getUserMedia({audio: false, video: true}, function (stream) {
     $('#video').prop('src', URL.createObjectURL(stream));
+    myStream = stream;
   }, function (error) {
     console.log(error);
+  });
+
+  var log = function (text) {
+    $('<p>').text(text).appendTo('#messages');
+  };
+
+  var showOtherStream = function (stream) {
+    $('#other-video').prop('src', URL.createObjectURL(stream));
+  };
+
+  var peer = new Peer({key: 'vvx00u5ssce1xlxr'});
+  peer
+    .on('open', function (id) {
+      log(id);
+    })
+    .on('call', function (call) {
+      call.answer(myStream);
+      call.on('stream', showOtherStream);
+    });
+
+  $(document).on('click', '#call', function () {
+    var call = peer.call($('#other-peerid').val(), myStream);
+    call.on('stream', showOtherStream);
   });
 });
